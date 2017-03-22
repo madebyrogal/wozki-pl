791c035af112225d1852a2273fde1cf844d25446
Tomasz Rogalski <tomasz@madebyrogal.com>
2017-03-22 08:03:41 +0100

    MOD Move Info on left side and remove gallery from new buy view

--- apps/frontend/modules/offer/templates/productDetailsSuccess.php
+++ apps/frontend/modules/offer/templates/productDetailsSuccess.php
@@ -1,28 +1,37 @@
 <div id="sHeader">
-  <div class="sHeaderTop">
-    <h1><?= $offer->getTitle() ?></h1>
-    <?php if (isset($category)): ?>
-      <?= include_component('offer', 'categories', array('activeCategoryId' => $category->getId())) ?>
-    <?php else: ?>
-      <?= include_component('offer', 'categories', array('activeCategoryId' => 'offer_promotion')) ?>
-    <?php endif; ?>
-    <?php include_component('search', 'showForm') ?>
-    <div class="clear"></div>
-  </div>
+    <div class="sHeaderTop">
+        <h1><?= $offer->getTitle() ?></h1>
+        <?php if (isset($category)): ?>
+            <?= include_component('offer', 'categories', array('activeCategoryId' => $category->getId())) ?>
+        <?php else: ?>
+            <?= include_component('offer', 'categories', array('activeCategoryId' => 'offer_promotion')) ?>
+        <?php endif; ?>
+        <?php include_component('search', 'showForm') ?>
+        <div class="clear"></div>
+    </div>
 </div>
 <div id="content">  
-  <div class="sTollbar">
-    <a class="back" title="<?= __("Powrót do listy")?>" href="javascript:history.back()"><?= __("Powrót do listy")?></a>
-  </div>
-  <div class="left">
-    <h2><?= $product->getName()?></h2>
-    <?= include_component('offer', 'showAttributes', array('product'=>$product))?>
-    <h3><?= __("Informacje dodatkowe")?></h3>
-    <?= $product->getInfo(ESC_RAW)?>
-  </div>
-  <?= include_component('offer', 'showGallery', array('product'=>$product))?>
-  <div class="clear"></div>
-  <div class="sTollbar">
-    <a class="back" title="<?= __("Powrót do listy")?>" href="javascript:history.back()"><?= __("Powrót do listy")?></a>               
-  </div>
+    <div class="sTollbar">
+        <a class="back" title="<?= __("Powrót do listy") ?>" href="javascript:history.back()"><?= __("Powrót do listy") ?></a>
+    </div>
+    <div class="left">
+        <h2><?= $product->getName() ?></h2>
+        <?= include_component('offer', 'showAttributes', array('product' => $product)) ?>
+        <?php if($category->getId() != 7): //new buy?>
+            <h3><?= __("Informacje dodatkowe") ?></h3>
+            <?= $product->getInfo(ESC_RAW) ?>
+        <?php endif; ?>
+    </div>
+    <?php if($category->getId() == 7): //new buy?>
+        <div class='left'>
+        <h3><?= __("Informacje dodatkowe") ?></h3>
+        <?= $product->getInfo(ESC_RAW) ?>
+        </div>
+    <?php else:?>
+        <?= include_component('offer', 'showGallery', array('product' => $product)) ?>
+    <?php endif; ?>
+    <div class="clear"></div>
+    <div class="sTollbar">
+        <a class="back" title="<?= __("Powrót do listy") ?>" href="javascript:history.back()"><?= __("Powrót do listy") ?></a>               
+    </div>
 </div>
